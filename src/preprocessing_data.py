73a9ff1ef250ded3c6e53e9b8a7c73a1829825ac
mariethoz <cedirc.mariethoz.cm@gmail.com>
2022-02-28 16:32:38 +0100

    Complete processing with polynomial function

--- src/preprocessing_data.py
+++ src/preprocessing_data.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
-
-def preprocessing_minMax(df):
+def preprocessing_min_max(df):
     """
     Give a scaler between 0 and 1 depending on the colum
     
@@ -17,8 +16,7 @@ def preprocessing_minMax(df):
     df[:] = preprocessing.MinMaxScaler().fit_transform(df[:])
     return df
 
-
-def preprocessing_zNormal(df):
+def preprocessing_z_normal(df):
     """
     Give the z-normalisation depending on the colum
     
@@ -32,16 +30,39 @@ def preprocessing_zNormal(df):
     df[:] = preprocessing.StandardScaler().fit_transform(df[:])
     return df
 
+def preprocessing_polynomial(df, degree=2):
+    """
+    Give the polynomial features depending on the colum
+    
+    Parameters:
+    df : pandas.DataFrame
+        Data we will processing
+    degree : int
+        Degree of polynome we will
+    Return:
+        pandas.DataFrame
+        Data processing
+    """
+    head = df.columns.tolist()
+    df = preprocessing.PolynomialFeatures(degree=degree).fit_transform(df[:])
+    df = pd.DataFrame(data=df)
+    for i in range(0,len(head)):
+        df.rename(columns={i+1: head[i]}, inplace=True)
+    return df
 
 if __name__ == '__main__':
     print("Test preprocessing data...")
     d = {'col1': [1, 2, 3, 4, 5, 6, 7], 'col2': [2, 2, 2, 2, 2, 2, 2], 'col3': [8, 9, 10, 11, 12, 13, 14]}
-    minMax = preprocessing_minMax(pd.DataFrame(data=d))
-    normal = preprocessing_zNormal(pd.DataFrame(data=d))
+    minMax = preprocessing_min_max(pd.DataFrame(data=d))
+    normal = preprocessing_z_normal(pd.DataFrame(data=d))
+    polyno = preprocessing_polynomial(pd.DataFrame(data=d),3)
     print("Test preprocessing minmax...")
     print(minMax)
     print("Test preprocessing z-normal...")
     print(normal)
+    print("Test preprocessing polynomial...")
+    np.set_printoptions(suppress=True)
+    print(polyno)
     print("Test preprocessing original data...")
     print(pd.DataFrame(data=d))
     
