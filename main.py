55f793d23bdc64f8c0eff35d759b15fcbaf8e6be
AdrienChassignet <a.chassignet@hotmail.fr>
2022-03-07 16:17:52 +0100

    Clean-up and start implementation of RegressionTrees in main

--- main.py
+++ main.py
@@ -3,11 +3,10 @@
 This script parse the command-line arguments to select block components of the 
 model fitting pipeline.
 
-@Author:    Adrien Chassignet
+@Author:    Adrien Chassignet, Cédric Mariéthoz
 @Date:      Feb 28 2022 
 @Version:   1.0
 """
-from statistics import mean
 import sys
 sys.path.insert(1, 'src')
 
@@ -18,10 +17,10 @@ from preprocessing_data import (get_polynomial_features,
                                 min_max_scaling,
                                 z_normalisation)
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import mean_absolute_error
 
 DATASETS = ['wine', 'houses']
-SEEDS = range(4)
 PREPROCESSING = ['min-max', 'z-normalisation']
 MODELS = ['linear-regression', 'regression-trees']
 METRICS = ['mae']
@@ -31,7 +30,7 @@ def get_cl_args():
     Parse the command line (sys.argv) to select elements of the pipeline.
 
     Return:
-    args:   class 'argparse.Namespace'
+    args : class 'argparse.Namespace'
         The populated Namespace of the arguments. Each string argument is an 
         attribute of the namespace.
     """
@@ -41,9 +40,11 @@ def get_cl_args():
     parser.add_argument('--dataset', action='store', choices=DATASETS,
                         help='Dataset to use between wine quality and Boston '
                         'house prices datasets.', default=DATASETS[0])
-    parser.add_argument('--seed', action='store', choices=SEEDS, type=int,
-                        help='Seed for the pseudo-RNG used to split the data',
-                        default=SEEDS[0])
+    parser.add_argument('--seed', action='store', type=int,
+                        help='Seed for the pseudo-RNG used to split the data '
+                        'and to initialize the models. '
+                        'If no seed is given by the user, the system will be '
+                        'fully random.', default=None)
     parser.add_argument('--scaling', action='store', choices=PREPROCESSING,
                         help='Select the scaling pre-processing technique to '
                         'apply to the features.', default=PREPROCESSING[0])
@@ -65,7 +66,7 @@ if __name__ == "__main__":
     args = get_cl_args()
 
     for arg in vars(args):
-        print(arg, getattr(args, arg))
+        print(arg, ": ", getattr(args, arg))
 
     # Load dataset
     # TODO: check if files has been downloaded and download if not the case
@@ -78,13 +79,14 @@ if __name__ == "__main__":
     if args.polynomial:
         data_train = get_polynomial_features(data_train)
         data_test = get_polynomial_features(data_test)
+
     if args.scaling == PREPROCESSING[0]:
         data_test, data_train = min_max_scaling(data_test, data_train)
     elif args.scaling == PREPROCESSING[1]:
         data_test, data_train = z_normalisation(data_test, data_train)
 
     # Train model
-    x_train = data_train.iloc[:, 0:-1]
+    x_train = data_train.iloc[:, :-1]
     y_train = data_train.iloc[:, -1]
     
     if args.model == MODELS[0]:
@@ -92,16 +94,19 @@ if __name__ == "__main__":
     elif args.model == MODELS[1]:
         print('Regression trees not implemented yet.')
         sys.exit()
-        # model = RegressionTrees()
+        #model = DecisionTreeClassifier(random_state=args.seed)
     
     model.fit(x_train, y_train)
 
     # Analyze data
-    x_test = data_test.iloc[:, 0:-1]
+    x_test = data_test.iloc[:, :-1]
     y_test = data_test.iloc[:, -1]
     # Inference
+    y_train_predict = model.predict(x_train)
     y_test_predict = model.predict(x_test)
     #Evaluation
     if args.metrics == METRICS[0]:
+        mae = mean_absolute_error(y_train, y_train_predict)
+        print("On the train set: \nMean absolute error= ", mae)
         mae = mean_absolute_error(y_test, y_test_predict)
-        print("Mean absolute error= ", mae)
+        print("On the test set: \nMean absolute error= ", mae)
