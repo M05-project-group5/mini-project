c8f7cf9da5d33732ccf4f7dc6f8d7c8d32c2ea1a
AdrienChassignet <a.chassignet@hotmail.fr>
2022-03-07 12:53:45 +0100

    New functions for specific dataset loading and docstring additions

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,4 @@
-import numpy as np
+import sys
 import pandas as pd
 
 VERBOSE = True
@@ -11,10 +11,66 @@ def read_data(func, *args, **kwargs):
     """
     Read data file with given parsing function and arguments.
     
+    Return:
+    df : pandas.DataFrame
+        Loaded data from the given file in arguments.
+
     Example:
         df = read_data(pd.read_csv, filepath_or_buffer=filename.csv, sep=';')
     """
     return func(*args, **kwargs)
+
+def load_wine_dataset():
+    """
+    Load the wine quality dataset from "downloads/winequality-red.csv" and 
+    "downloads/winequality-white.csv". Both dataset are merged.
+
+    Return:
+    df : pandas.DataFrame
+        Wine quality loaded data
+    """
+    # Read the two type of wine datasets
+    redwine_df = read_data(pd.read_csv, redwine_file, sep=';')
+    whitewine_df = read_data(pd.read_csv, whitewine_file, sep=';')
+    # Concatenate the two type of wine datasets
+    wine_df = pd.concat([redwine_df, whitewine_df])
+    # Quality check
+    quality_check(wine_df)
+    # Remove wines with unvalid quality scores
+    wine_df = wine_df[wine_df.iloc[:, -1].between(0, 10, inclusive='both')]
+
+    return wine_df
+
+def load_houses_dataset():
+    """
+    Load the Boston house prices dataset from "downloads/housing.data".
+
+    Return:
+    df : pandas.DataFrame
+        Boston house prices loaded data
+    """
+    # Read the two type of wine datasets
+    houses_df = read_data(pd.read_csv, houses_file, sep='\s+', header=None)
+    # Quality check
+    quality_check(houses_df)
+
+    return houses_df
+
+def load_dataset(name='wine'):
+    """
+    Load the dataset given by its name 'wine' or 'houses'.
+
+    Return:
+    df : pandas.DataFrame
+        Loaded dataset.
+    """
+    if name == 'wine':
+        return load_wine_dataset()
+    elif name == 'houses':
+        return load_houses_dataset()
+    else:
+        print("Dataset name unknown.")
+        sys.exit()
 
 def quality_check(data):
     """
