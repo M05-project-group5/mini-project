d4e35272e33959091fb99236aba607441c401d4d
AdrienChassignet <a.chassignet@hotmail.fr>
2022-03-21 18:57:08 +0100

    Fix status and proper removal of existing dataset files before corresponding unit tests

--- test/test_data_loading.py
+++ test/test_data_loading.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
-sys.path.insert(1, './src')
 sys.path.insert(1, '.')
+sys.path.insert(1, 'src/')
 
 import load_data
 import download_datasets
@@ -18,10 +18,14 @@ def test_download_wine():
     """ Test if wine quality datasets are correctly downloaded. """
     path_redwine = "downloads/winequality-red.csv"
     path_whitewine = "downloads/winequality-white.csv"
-    if os.path.exists(path_redwine):
+    try:
         os.remove(path_redwine)
-    if os.path.exists(path_whitewine):
+    except OSError:
+        pass
+    try:
         os.remove(path_whitewine)
+    except OSError:
+        pass
 
     download_datasets.download_wine()
     assert os.path.exists(path_redwine), "Red Wine Quality dataset not downloaded."
@@ -31,8 +35,10 @@ def test_download_wine():
 def test_download_houses():
     """ Test if Boston house prices dataset is correctly downloaded. """
     path_houses = "downloads/housing.data"
-    if os.path.exists(path_houses):
+    try:
         os.remove(path_houses)
+    except OSError:
+        pass
 
     download_datasets.download_houses()
     assert os.path.exists(path_houses), "Boston house prices dataset not downloaded."
