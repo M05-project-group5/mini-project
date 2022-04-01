692783b51675d42db052e394dfc2b7320b42fd1a
mariethoz <cedirc.mariethoz.cm@gmail.com>
2022-04-01 16:16:58 +0200

    Modify setup and ci, and add badges for git

--- README.rst
+++ README.rst
@@ -2,6 +2,8 @@
    :target: https://github.com/M05-project-group5/mini-project/actions/workflows/ci-testing.yml
 .. image:: https://coveralls.io/repos/github/M05-project-group5/mini-project/badge.svg?branch=main
    :target: https://coveralls.io/github/M05-project-group5/mini-project?branch=main
+.. image:: https://img.shields.io/github/v/tag/M05-project-group5/mini-project?branch=main
+   :target: https://github.com/M05-project-group5/mini-project
 .. image:: https://img.shields.io/badge/docs-latest-orange.svg
    :target: https://M05-project-group5.github.io/mini-project/index.html
 .. image:: https://img.shields.io/badge/pypi-project-blueviolet.svg
--- setup.py
+++ setup.py
@@ -7,22 +7,25 @@ from setuptools import setup, find_packages
 def load_requirements(f):
     retval = [str(k.strip()) for k in open(f, "rt")]
     return [k for k in retval if k and k[0] not in ("#", "-")]
-    
+
+
 setup(
     name="Mini-Project",
-    version="v0.0.2",
-    author="CA, MC",
-    description="Mini-Project for the Master in IA to Idiap",
+    version="0.0.0",
+    description="Basic example of a Reproducible Research Project in Python",
+    url="https://github.com/M05-project-group5/mini-project",
+    license="BSD",
+    author="Adrien Chassignet, Cédric Mariéthoz",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
-    url="https://github.com/M05-project-group5/mini-project",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=load_requirements("requirements.txt"),
+    entry_points={"console_scripts": ["mini-project-main = main:main"]},
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    python_requires=">=3.6",
 )
