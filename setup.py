a63323bf570c7aec0b9d6ec03e7b2b40ed175484
mariethoz <cedirc.mariethoz.cm@gmail.com>
2022-04-04 16:09:36 +0200

    Fix subprocess problem

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # coding=utf-8
 
 from setuptools import setup, find_packages
-import subprocess
 
 def load_requirements(f):
     retval = [str(k.strip()) for k in open(f, "rt")]
@@ -11,7 +10,7 @@ def load_requirements(f):
 
 setup(
     name="Mini-Project",
-    version="1.0.0a",
+    version="1.0.0a1",
     description="Basic example of a Reproducible Research Project in Python",
     url="https://github.com/M05-project-group5/mini-project",
     license="BSD",
